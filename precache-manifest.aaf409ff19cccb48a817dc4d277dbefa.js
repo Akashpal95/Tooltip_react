527a3f4ef9fe20234e2acfc24978a07989b6a21e
Akashpal95 <akash.pal95@gmail.com>
2020-08-09 22:51:41 +0530

    Updates

---
+++ precache-manifest.aaf409ff19cccb48a817dc4d277dbefa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc73413cbe25c5733f598d6568de7ade",
+    "url": "/Tooltip_react/index.html"
+  },
+  {
+    "revision": "e28d98afffa03fb794f0",
+    "url": "/Tooltip_react/static/css/main.0470cb58.chunk.css"
+  },
+  {
+    "revision": "39f2c1537d206e9168ea",
+    "url": "/Tooltip_react/static/js/2.0e2e7a76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tooltip_react/static/js/2.0e2e7a76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e28d98afffa03fb794f0",
+    "url": "/Tooltip_react/static/js/main.c9c7d558.chunk.js"
+  },
+  {
+    "revision": "28ba30de56e5ab0f668a",
+    "url": "/Tooltip_react/static/js/runtime-main.b3fa3114.js"
+  }
+]);
